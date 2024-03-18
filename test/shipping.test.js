204555468b036856fb5d65fc8866e9332df0c2bd
Sriram Sivarajah <sriramsivarajah@gmail.com>
2024-03-18 15:02:50 +0000

    write unit tests for all scenarios in regressions

---
+++ test/shipping.test.js
@@ -0,0 +1,18 @@
+import { describe, expect, it } from "vitest";
+import { calculateShipping } from "../shipping";
+
+describe("calculateShipping", () => {
+  it.each([
+    ["UK", 99.99, 4.99],
+    ["UK", 100, 0],
+    ["EU", 99.99, 8.99],
+    ["EU", 100, 4.99],
+    ["OTHER", 100, 9.99],
+  ])(
+    "should calculate the correct shipping costs for region: %s, total: %i",
+    async (region, total, expectedCost) => {
+      const shippingCost = await calculateShipping(region, total);
+      expect(shippingCost).toBe(expectedCost);
+    }
+  );
+});
