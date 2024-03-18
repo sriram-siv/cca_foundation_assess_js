c6a0c656758b9ea14ff03f01e05eb115870935d0
Sriram Sivarajah <sriramsivarajah@gmail.com>
2024-03-18 14:24:36 +0000

    convert to promise chain

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const calculateShipping = require("./shipping");
 
 export async function printShippingCosts(country, orderTotal) {
-  const shippingCost = await calculateShipping(country, orderTotal);
-
-  console.log(
-    `Shipping cost to ${country} for order total £${orderTotal} is £${shippingCost}`
+  calculateShipping(country, orderTotal).then((shippingCost) =>
+    console.log(
+      `Shipping cost to ${country} for order total £${orderTotal} is £${shippingCost}`
+    )
   );
 }
