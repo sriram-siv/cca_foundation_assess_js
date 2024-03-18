3603eb1631bca9497f8e6390a124a430cc84c626
Sriram Sivarajah <sriramsivarajah@gmail.com>
2024-03-18 15:21:47 +0000

    expect plain value from shipping function

--- main.js
+++ main.js
@@ -3,15 +3,13 @@ import { calculateShipping, getCountryRegion } from "./shipping";
 export async function printShippingCosts(country, orderTotal) {
   const region = await getCountryRegion(country).catch(console.error);
 
-  if (region) {
-    calculateShipping(region, orderTotal)
-      .then((shippingCost) => {
-        return console.log(
-          `Shipping cost to ${country} for order total £${orderTotal} is £${shippingCost}`
-        );
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+  try {
+    const shippingCost = calculateShipping(region, orderTotal);
+
+    console.log(
+      `Shipping cost to ${country} for order total £${orderTotal} is £${shippingCost}`
+    );
+  } catch (error) {
+    console.error(error);
   }
 }
