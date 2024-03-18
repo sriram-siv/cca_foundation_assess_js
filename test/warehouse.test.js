bd3e42366808da834454fe08c562412ad70d6d4b
Sriram Sivarajah <sriramsivarajah@gmail.com>
2024-03-18 15:43:36 +0000

    test for receiving stock on existing or new products

--- test/warehouse.test.js
+++ test/warehouse.test.js
@@ -8,12 +8,23 @@ describe("Warehouse", () => {
     expect(warehouse.productStockDictionary).toEqual({});
   });
 
-  it("should be able to receive stock", () => {
+  it("should be able to receive stock for a new product", () => {
     const warehouse = new Warehouse();
     const product = new Product("test", "Test Product", 100);
 
     warehouse.receiveStock(product, 20);
 
+    expect(warehouse.productStockDictionary.test).toBe(20);
+  });
+
+  it("should be able to receive stock for an existing product", () => {
+    const warehouse = new Warehouse();
+    const product = new Product("test", "Test Product", 100);
+
+    warehouse.receiveStock(product, 0);
+    expect(warehouse.productStockDictionary.test).toBe(0);
+
+    warehouse.receiveStock(product, 20);
     expect(warehouse.productStockDictionary.test).toBe(20);
   });
 });
