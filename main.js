e0c572a499780247e4bc40c0f7ffdeb0bfaff9d1
Sriram Sivarajah <sriramsivarajah@gmail.com>
2024-03-18 14:15:23 +0000

    Paramaterise tests. Call examples from test instead of main function

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const calculateShipping = require("./shipping");
-const Country = require("./countries");
 
 export function printShippingCosts(country, orderTotal) {
   function callback(shipping) {
@@ -9,10 +8,3 @@ export function printShippingCosts(country, orderTotal) {
   }
   calculateShipping(country, orderTotal, callback);
 }
-
-printShippingCosts(Country.UNITED_KINGDOM, 99.99);
-printShippingCosts(Country.UNITED_KINGDOM, 100.0);
-printShippingCosts(Country.FRANCE, 99.99);
-printShippingCosts(Country.FRANCE, 100.0);
-printShippingCosts(Country.ALBANIA, 99.99);
-printShippingCosts(Country.ALBANIA, 100.0);
--- test/data_regression.test.js
+++ test/data_regression.test.js
@@ -1,29 +1,52 @@
-import { describe, expect, it, vi } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import { printShippingCosts } from "../main";
+import Country from "../countries";
 
 const spyConsole = vi.spyOn(console, "log");
 
 describe("printShippingCosts", () => {
-  it("should return results unchanged from initial outputs", async () => {
-    await new Promise((res) => setTimeout(res, 1000));
-
-    expect(spyConsole).toHaveBeenCalledWith(
-      "Shipping cost to United Kingdom for order total £99.99 is £4.99"
-    );
-    expect(spyConsole).toHaveBeenCalledWith(
-      "Shipping cost to United Kingdom for order total £100 is £0"
-    );
-    expect(spyConsole).toHaveBeenCalledWith(
-      "Shipping cost to France for order total £100 is £4.99"
-    );
-    expect(spyConsole).toHaveBeenCalledWith(
-      "Shipping cost to Albania for order total £99.99 is £9.99"
-    );
-    expect(spyConsole).toHaveBeenCalledWith(
-      "Shipping cost to United Kingdom for order total £99.99 is £4.99"
-    );
-    expect(spyConsole).toHaveBeenCalledWith(
-      "Shipping cost to France for order total £99.99 is £8.99"
-    );
+  beforeEach(() => {
+    spyConsole.mockClear();
   });
+
+  it.each([
+    [
+      Country.UNITED_KINGDOM,
+      100.0,
+      "Shipping cost to United Kingdom for order total £100 is £0",
+    ],
+    [
+      Country.UNITED_KINGDOM,
+      99.99,
+      "Shipping cost to United Kingdom for order total £99.99 is £4.99",
+    ],
+    [
+      Country.FRANCE,
+      99.99,
+      "Shipping cost to France for order total £99.99 is £8.99",
+    ],
+    [
+      Country.FRANCE,
+      100.0,
+      "Shipping cost to France for order total £100 is £4.99",
+    ],
+    [
+      Country.ALBANIA,
+      99.99,
+      "Shipping cost to Albania for order total £99.99 is £9.99",
+    ],
+    [
+      Country.ALBANIA,
+      100.0,
+      "Shipping cost to Albania for order total £100 is £9.99",
+    ],
+  ])(
+    "should return results unchanged from initial outputs",
+    async (country, orderTotal, output) => {
+      printShippingCosts(country, orderTotal);
+      await new Promise((res) => setTimeout(res, 1000));
+
+      expect(spyConsole).toHaveBeenCalledWith(output);
+    }
+  );
 });
