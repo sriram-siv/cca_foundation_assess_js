5406f98c9e698dbe36dc7992f757e55347c0fa1a
Sriram Sivarajah <sriramsivarajah@gmail.com>
2024-03-18 14:29:33 +0000

    extract get region function

--- shipping.js
+++ shipping.js
@@ -1,6 +1,6 @@
 const https = require("https");
 
-async function calculateShipping(country, orderTotal) {
+async function getCountryRegion(country) {
   const url = `https://npovmrfcyzu2gu42pmqa7zce6a0zikbf.lambda-url.eu-west-2.on.aws/?country=${country}`;
 
   return new Promise((res, rej) => {
@@ -16,24 +16,8 @@ async function calculateShipping(country, orderTotal) {
         // The whole response has been received. Process the data.
         resp.on("end", () => {
           try {
-            const region = JSON.parse(data).region;
-            let shipping = 0.0;
-
-            if (region === "UK") {
-              if (orderTotal < 100.0) {
-                shipping = 4.99;
-              }
-            } else if (region === "EU") {
-              if (orderTotal < 100) {
-                shipping = 8.99;
-              } else {
-                shipping = 4.99;
-              }
-            } else if (region === "OTHER") {
-              shipping = 9.99;
-            }
-
-            res(shipping);
+            const { region } = JSON.parse(data);
+            res(region);
           } catch (error) {
             rej(error);
           }
@@ -42,6 +26,32 @@ async function calculateShipping(country, orderTotal) {
       .on("error", (error) => {
         rej(error);
       });
+  });
+}
+
+async function calculateShipping(country, orderTotal) {
+  return new Promise((res, rej) => {
+    getCountryRegion(country)
+      .then((region) => {
+        let shipping = 0.0;
+
+        if (region === "UK") {
+          if (orderTotal < 100.0) {
+            shipping = 4.99;
+          }
+        } else if (region === "EU") {
+          if (orderTotal < 100) {
+            shipping = 8.99;
+          } else {
+            shipping = 4.99;
+          }
+        } else if (region === "OTHER") {
+          shipping = 9.99;
+        }
+
+        res(shipping);
+      })
+      .catch((error) => rej(error));
   });
 }
 
