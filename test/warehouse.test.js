32b7781097ed33bc04bb6353a22fdaafee154540
Sriram Sivarajah <sriramsivarajah@gmail.com>
2024-03-18 15:51:58 +0000

    add adjustStock function with tests

--- test/warehouse.test.js
+++ test/warehouse.test.js
@@ -27,4 +27,30 @@ describe("Warehouse", () => {
     warehouse.receiveStock(product, 20);
     expect(warehouse.productStockDictionary.test).toBe(20);
   });
+
+  it("should be able to adjust stock for a product", () => {
+    const warehouse = new Warehouse();
+    const product = new Product("test", "Test Product", 100);
+
+    warehouse.receiveStock(product, 50);
+    warehouse.adjustStock(product, 20);
+
+    expect(warehouse.productStockDictionary.test).toBe(30);
+  });
+
+  it("should not be able to adjust stock for a product by more than its available quantity", () => {
+    const warehouse = new Warehouse();
+    const product = new Product("test", "Test Product", 100);
+
+    warehouse.receiveStock(product, 50);
+
+    expect(() => warehouse.adjustStock(product, 100)).toThrow();
+  });
+
+  it("should not be able to adjust stock for a product not in the inventory", () => {
+    const warehouse = new Warehouse();
+    const product = new Product("test", "Test Product", 100);
+
+    expect(() => warehouse.adjustStock(product, 100)).toThrow();
+  });
 });
--- warehouse.js
+++ warehouse.js
@@ -20,4 +20,21 @@ export class Warehouse {
     this.productStockDictionary[product.id] ??= 0;
     this.productStockDictionary[product.id] += quantity;
   }
+
+  adjustStock(product, quantity) {
+    const currentStock = this.productStockDictionary[product.id];
+
+    if (currentStock === undefined) {
+      throw new Error(
+        "Attempting to adjust stock of product that does not exist in warehouse"
+      );
+    }
+    if (currentStock < quantity) {
+      throw new Error(
+        "Attempting to adjust stock by more than available amount"
+      );
+    }
+
+    this.productStockDictionary[product.id] = currentStock - quantity;
+  }
 }
